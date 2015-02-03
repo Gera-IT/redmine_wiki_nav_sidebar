786de19ee19e2e1fa3e6bbd4bd47dac4fee6cafa
Alex Sinelnikov <sinelniko@gmail.com>
2015-02-03 14:31:37 +0200

    removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 # See: http://guides.rubyonrails.org/routing.html
 
 resources :wiki_tree, only: :index
-get 'wiki_history', to: "wiki_tree#history"
-
-get '/projects/:project_id/wiki/:id/:version/wiki_diff', to: 'wiki_tree#wiki_diff', as: :wiki_diff_page
-get '/projects/:project_id/wiki/:id/wiki_diff', to: 'wiki_tree#wiki_diff', as: :wiki_diff_page
+# get 'wiki_history', to: "wiki_tree#history"
+#
+# get '/projects/:project_id/wiki/:id/:version/wiki_diff', to: 'wiki_tree#wiki_diff', as: :wiki_diff_page
+# get '/projects/:project_id/wiki/:id/wiki_diff', to: 'wiki_tree#wiki_diff', as: :wiki_diff_page
